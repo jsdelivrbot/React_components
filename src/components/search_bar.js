c8766e9f9567b0e807d4727f3c8bdce97340d0a8
lupan.daniel <lupan.daniel@gmail.com>
2017-07-12 22:02:53 +0300

    update input value on component rerender

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,15 +10,18 @@ class SearchBar extends Component {
 
   constructor(props) {
     super(props);                         // Component has it's own constructor function
-    this.state = { searchTerm: ''};       // initialize it with a new object and will contain properties to be recorded
+    this.state = { searchTerm: 'startging value'};       // initialize it with a new object and will contain properties to be recorded
   }
 
   render() {
     return (
       <div>
-        <input onChange={(event) => {     // updating the state forces the component and its children to rerender
-          this.setState({searchTerm: event.target.value}); // changing component state
-        }} />
+        <input
+          value={this.state.searchTerm}   // Turns input into a controlled component
+          onChange={(event) => {          // This.setState causes the component to rerender and after that,
+                                          // the value of the input gets updated
+          this.setState({searchTerm: event.target.value}); // the value of the input has not changed ATM
+          }} />
         Value of the input: {this.state.searchTerm}
       </div>
     );
