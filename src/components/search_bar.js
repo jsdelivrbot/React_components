505697fccc76f06f93f905ddb2dde6ceb8b87d5d
lupan.daniel <lupan.daniel@gmail.com>
2017-07-12 21:38:50 +0300

    added constructor and  state to the class based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,22 @@
 "use strict";
-import React,  { Component } from 'react'; // import React and pull off the property Component as variable Component
+import React, { Component } from 'react'; // import React and pull off the property Component as variable Component
 
 class SearchBar extends Component {
+
+  /*
+    state is a plane javascript object that exists on any component that is a class based component. Each instance of
+    class based component has it's own copy of state
+   */
+
+  constructor(props) {
+    super(props);                         // Component has it's own constructor function
+    this.state = { searchTerm: ''};       // initialize it with a new object and will contain properties to be recorded
+  }
+
   render() {
-    return <input onChange={this.onInputChange} />; // call function without brackets
+    return <input onChange={(event) => console.log(event.target.value)} />; // call function without brackets
   };
 
-  onInputChange(event) { // on/handle + name of element + name of the event
-    console.log(event.target.value);
-  };
 }
 
 export default SearchBar;
