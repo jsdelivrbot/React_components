24e0d61c6d2a90f754cd2d5ff80f20aa835710a6
lupan.daniel <lupan.daniel@gmail.com>
2017-07-12 21:27:29 +0300

    added element event handler for the search_bar component input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,14 @@
 "use strict";
 import React,  { Component } from 'react'; // import React and pull off the property Component as variable Component
 
-class SearchBar extends React {
+class SearchBar extends Component {
   render() {
-    return <input />;
-  }
+    return <input onChange={this.onInputChange} />; // call function without brackets
+  };
+
+  onInputChange(event) { // on/handle + name of element + name of the event
+    console.log(event.target.value);
+  };
 }
 
 export default SearchBar;
