d423509234ded91a46a9b9c60d740f3380e439f7
lupan.daniel <lupan.daniel@gmail.com>
2017-07-13 10:02:55 +0300

    loading youtube videos into the component state

--- src/index.js
+++ src/index.js
@@ -10,20 +10,16 @@ import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDCSN075ONuXRZVa_3mkbIj0oPDHqW5gLk';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
-// JSX, not HTML. Babbel and webpack transpile it to vanilla JavaScript
-// JSX cannot be interpreted by the browser
-
-/*
- Refactor the functional component(App) to a class base component to keep track of the list of videos by
- recording them on its state
-
- */
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    // this.setState({videos: videos})
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => this.setState({ videos }) )
+  }
+
   render() {
     return (
       <div>
@@ -32,9 +28,6 @@ class App extends Component {
     )
   }
 }
-
-// Take this component's generated HTML and put it on
-// The page (in the Document Object Model)
 
 ReactDOM.render(<App />, document.querySelector('.container'));
 
