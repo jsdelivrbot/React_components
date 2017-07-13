9066ff5c28f1b4a79f1913a52c6b63a2d58092f6
lupan.daniel <lupan.daniel@gmail.com>
2017-07-13 14:30:07 +0300

    solve undefined object in video_detail before content gets loaded

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
 
 const API_KEY = 'AIzaSyDCSN075ONuXRZVa_3mkbIj0oPDHqW5gLk';
 
@@ -27,6 +28,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     )
